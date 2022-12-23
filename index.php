12877da371db2afe926a3461f1091600c03f4cee
Harshal Lad <115536696+Harshal192@users.noreply.github.com>
2022-12-23 15:21:36 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,30 +1,85 @@
+<html>
 <?php
 session_start();
-if(Isset($_SESSION['user'])){
+if (isset($_SESSION['user'])) {
 ?>
-<h1>This is the LOGGED-IN page</h1>
-<button onclick="window.location.href='logout.php'" type="button">Logout</button>
+    <h1>This is the LOGGED-IN page</h1>
+
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Log out
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <label for="name">Name</label>
+    <input type="text" id="name">
+    <button type="button" id="status" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#disname">
+        Open
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="disname" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Open</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <script>
+                        jQuery(document).ready(function() {
+                            jQuery("#status").on('click', function() {
+                                var inputtagvalue = jQuery('#name').val();
+                                jQuery('.modal-body').html(inputtagvalue);
+                            });
+                        });
+                    </script>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <?php
-}else{
+} else {
 ?>
-<h1>This is not the LOGGED-IN page</h1>
+    <h2>This is not the LOGGED-IN page</h2>
+    <button type="button" herf="http://localhost/internship2-Harshal/login.php">Login</button>
 <?php
 }
 ?>
-<html>
-
-<body>
-    <style>
-        h1 {
-            background-color: aquamarine;
-            color: red;
-        }
-    </style>
-    <h1>Hallo</h1>
-    <?php
-    echo "Za Worldo";
-    ?>
-
-</body>
+<h1>Hello</h1>
+<?php
+echo "Za Worldo";
+?>
 
 </html>
